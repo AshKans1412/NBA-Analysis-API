105f18f4896f52ff3a513636e5eb25288821fc46
AshKans1412 <71004480+AshKans1412@users.noreply.github.com>
2023-11-23 23:29:08 -0700

    Create Data_Scraping.py
    Created a Data Scrapping Functionality

---
+++ Assests/Data_Scraping.py
@@ -0,0 +1,23 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def data_scrapper():
+  url = "https://www.basketball-reference.com/leagues/NBA_2024_per_game.html"
+  response = requests.get(url)
+  soup = BeautifulSoup(response.content, "html.parser")
+  table = soup.find("table", {"id": "per_game_stats"})
+  
+  
+  headers = [ "Player", "Pos", "Age", "Tm", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%",
+             "2P", "2PA", "2P%", "eFG%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK",
+             "TOV", "PF", "PTS"]
+  
+  # Extract the table rows
+  rows = []
+  for row in table.find_all("tr"):
+      data = [cell.get_text() for cell in row.find_all("td")]
+      if data:
+          rows.append(data)
+  
+  return df 
