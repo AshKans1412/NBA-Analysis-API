e4ec1f9e65c26015237a6101cab3731db94ff6b7
Ash Kans <71004480+AshKans1412@users.noreply.github.com>
2023-11-25 19:35:54 -0700

    Update ML_Accesspoint.py

--- Assests/ML_Accesspoint.py
+++ Assests/ML_Accesspoint.py
@@ -12,7 +12,7 @@ svm_file = "Models/model.sav"
 #scaler = pickle.load(open(scaler_file,"rb"))
 
 
-def predict_2(team1, team2, model, label_encoder, scaler, data):    
+def predict_2(team1, team2, model, scaler, data):    
     mapping = {'ATL': 0, 'BOS': 1, 'BRK': 2, 'CHI': 3, 'CHO': 4, 'CLE': 5, 'DAL': 6, 'DEN': 7, 'DET': 8, 'GSW': 9, 'HOU': 10, 'IND': 11, 'LAC': 12, 'LAL': 13, 'MEM': 14, 'MIA': 15, 'MIL': 16, 'MIN': 17, 'NOP': 18, 'NYK': 19, 'OKC': 20, 'ORL': 21, 'PHI': 22, 'PHO': 23, 'POR': 24, 'SAC': 25, 'SAS': 26, 'TOR': 27, 'UTA': 28, 'WAS': 29}
 
     team1_encoded = mapping[team1]
